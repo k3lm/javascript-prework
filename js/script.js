65640999916812a4c5b5637c00e684a778d64e81
Mateusz Mach <matmach99@gmail.com>
2023-07-05 14:24:41 +0200

    Change player input to buttons

--- js/script.js
+++ js/script.js
@@ -1,53 +1,76 @@
-var argMoveId,
-  argPlayerMove,
-  argComputerMove,
-  computerMove,
-  playerMove,
-  randomNumber,
-  playerInput
+var argButtonName,
+  // buttonTest = document.getElementById('button-test'),
+  buttonRock = document.getElementById('button-rock'),
+  buttonPaper = document.getElementById('button-paper'),
+  buttonScissors = document.getElementById('button-scissors')
 
-function getMoveName(argMoveId) {
-  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId)
-  if (argMoveId == 1) {
-    return 'kamień'
-  } else if (argMoveId == 2) {
-    return 'papier'
-  } else if (argMoveId == 3) {
-    return 'nożyce'
-  } else {
-    printMessage(
-      'Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".'
+function buttonClicked(argButtonName) {
+  clearMessages()
+  console.log(argButtonName + ' został kliknięty')
+  var argMoveId,
+    argPlayerMove,
+    argComputerMove,
+    computerMove,
+    playerMove,
+    randomNumber,
+    playerInput
+
+  function getMoveName(argMoveId) {
+    console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId)
+    if (argMoveId == 1) {
+      return 'kamień'
+    } else if (argMoveId == 2) {
+      return 'papier'
+    } else if (argMoveId == 3) {
+      return 'nożyce'
+    } else {
+      printMessage(
+        'Nie znam ruchu o id ' +
+          argMoveId +
+          '. Zakładam, że chodziło o "kamień".'
+      )
+      return 'kamień'
+    }
+  }
+
+  function displayResult(argPlayerMove, argComputerMove) {
+    console.log(
+      'wywołano funkcję displayResults z argumentami: ' +
+        argPlayerMove +
+        ', ' +
+        argComputerMove
     )
-    return 'kamień'
+    if (
+      (argPlayerMove == 'papier' && argComputerMove == 'kamień') ||
+      (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') ||
+      (argPlayerMove == 'nożyce' && argComputerMove == 'papier')
+    ) {
+      printMessage('Wygrywasz :)')
+    } else if (argPlayerMove == argComputerMove) {
+      printMessage('Remis :|')
+    } else {
+      printMessage('Przegrałeś :(')
+    }
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove)
   }
+  playerMove = argButtonName
+  console.log('ruch gracza to: ' + playerMove)
+  randomNumber = Math.floor(Math.random() * 3 + 1)
+  console.log('wylosowana liczba to: ' + randomNumber)
+  computerMove = getMoveName(randomNumber)
+  console.log('ruch komputera to: ' + computerMove)
+  displayResult(playerMove, computerMove)
 }
 
-function displayResult(argPlayerMove, argComputerMove) {
-  console.log(
-    'wywołano funkcję displayResults z argumentami: ' +
-      argPlayerMove +
-      ', ' +
-      argComputerMove
-  )
-  if (
-    (argPlayerMove == 'papier' && argComputerMove == 'kamień') ||
-    (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') ||
-    (argPlayerMove == 'nożyce' && argComputerMove == 'papier')
-  ) {
-    printMessage('Wygrywasz :)')
-  } else if (argPlayerMove == argComputerMove) {
-    printMessage('Remis :|')
-  } else {
-    printMessage('Przegrałeś :(')
-  }
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove)
-}
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
-console.log('wybór ruchu gracza to: ' + playerInput)
-playerMove = getMoveName(playerInput)
-console.log('ruch gracza to: ' + playerMove)
-randomNumber = Math.floor(Math.random() * 3 + 1)
-console.log('wylosowana liczba to: ' + randomNumber)
-computerMove = getMoveName(randomNumber)
-console.log('ruch komputera to: ' + computerMove)
-displayResult(playerMove, computerMove)
+// buttonTest.addEventListener('click', function () {
+//   buttonClicked('Guzik TEST')
+// })
+buttonRock.addEventListener('click', function () {
+  buttonClicked('kamień')
+})
+buttonPaper.addEventListener('click', function () {
+  buttonClicked('papier')
+})
+buttonScissors.addEventListener('click', function () {
+  buttonClicked('nożyce')
+})
